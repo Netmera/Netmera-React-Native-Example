861780f6c26ac9158fe6cc5aed80aeefe8ea15dd
Baki Güneş <baki@initialcode.com.tr>
2022-07-15 21:09:59 +0300

    - Media Push integration was done.

---
+++ ios/NetmeraReactNativeNotificationContentExtension/NetmeraReactNativeNotificationContentExtension-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  NetmeraReactNativeNotificationContentExtension-Bridging-Header.h
+//  NetmeraReactNativeExample
+//
+//  Created by Inomera Research on 15.07.2022.
+//
+
+#ifndef NetmeraReactNativeNotificationContentExtension_Bridging_Header_h
+#define NetmeraReactNativeNotificationContentExtension_Bridging_Header_h
+
+#import <NetmeraNotificationContentExtension/NetmeraNotificationContentExtension.h>
+
+#endif /* NetmeraReactNativeNotificationContentExtension_Bridging_Header_h */
+
